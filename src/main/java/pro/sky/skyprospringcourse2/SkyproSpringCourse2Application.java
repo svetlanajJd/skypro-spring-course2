c8c3c48fb127079f99239b07e04897289d5f7026
sveta <svetlana.fedoseeva.88@mail.ru>
2022-08-17 16:12:16 +0300

    первый коммит

---
+++ src/main/java/pro/sky/skyprospringcourse2/SkyproSpringCourse2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcourse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCourse2Application.class, args);
+	}
+
+}
